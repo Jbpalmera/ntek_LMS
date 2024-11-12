1ba0dd012353cb6edd7bec7c031d4329d9c968ce
jbpalmera <palmerajb15@gmail.com>
2024-11-12 09:18:25 +0800

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.103/LibraryManagement-';
-// $config['base_url'] = 'http://localhost/LMS';
+// $config['base_url'] = 'http://192.168.1.103/LibraryManagement-';
+$config['base_url'] = 'http://localhost/jb-project/LMS';
 // $config['base_url'] = '';
 
 /*
